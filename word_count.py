a79efd709ffa7cb96fc4d74e5538ac574a8fcc3b
Hillary <hmiller@hsph.harvard.edu>
2018-10-04 12:28:37 -0400

    open and read lines of file

--- word_count.py
+++ word_count.py
@@ -18,10 +18,18 @@ parser.add_argument(
 
 args = parser.parse_args( )
 
-print(args) #returns Namespace, which indicates type of variable
-print(args.data_file) #make sure it's reading in the data like it should
+print("args =", args) #returns Namespace, which indicates type of variable
+print("args data file =", args.data_file) #make sure it's reading in the data like it should
+
+#fh = file handle
+fh = open(args.data_file)
+print("the file handle is", fh)
 
 
+#count variables - set = 0 
+lines = 0
+words = 0
+chars = 0
 #-------------------------------------------------------------------------------
 # our code for analyzing the data
 #-------------------------------------------------------------------------------
