8366311bc2ff7ce03bee0981d2ca14027064d932
Hillary <hmiller@hsph.harvard.edu>
2018-10-04 12:17:04 -0400

    set file path

--- word_count.py
+++ word_count.py
@@ -4,16 +4,23 @@ import argparse
 
 parser = argparse.ArgumentParser( description="" )
 
+
 parser.add_argument(
-	"arg",
-	help="what does this do?",
+	"data_file",
+	help = "path to the file we want to read",
 )
+
+
 
 #-------------------------------------------------------------------------------
 # Are there other arguments we need?
 #-------------------------------------------------------------------------------
 
 args = parser.parse_args( )
+
+print(args) #returns Namespace, which indicates type of variable
+print(args.data_file) #make sure it's reading in the data like it should
+
 
 #-------------------------------------------------------------------------------
 # our code for analyzing the data
